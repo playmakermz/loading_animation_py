5b53dceb3ac0c9586e586eb1a9022f51e1b2390a
playmakermz <60807663+playmakermz@users.noreply.github.com>
2020-03-07 19:45:51 -0800

    Add files via upload

---
+++ animation.py
@@ -0,0 +1,59 @@
+import time
+
+print("""
+
+
+    .                                
+,-. |  ,-. . . ,-,-. ,-. . , ,-. ,-. 
+| | |  ,-| | | | | | ,-| |/  |-' |   
+|-' `' `-^ `-| ' ' ' `-^ |\  `-' '   
+|           /|           ' `         
+'          `-'                       
+
+
+""")
+
+while True:
+    for i in range (1,2):
+        print("""
+                
+.            .             .  
+|  ,-. ,-. ,-| . ,-. ,-.    \ 
+|  | | ,-| | | | | | | | -- / 
+`' `-' `-^ `-' ' ' ' `-|   '  
+                      ,|      
+                      `'      
+""")
+
+        print("0-")
+        time.sleep(2)
+        print("0-0")
+        time.sleep(2)
+        print("0-0-")
+        time.sleep(2)
+        print("0-0-0")
+        time.sleep(2)
+        print("0-0-0-")
+        time.sleep(2)
+        print("0-0-0-0")
+        time.sleep(2)
+        print("0-0-0-0-")
+        time.sleep(2)
+        print("0-0-0-0-0")
+        time.sleep(2)
+        print("0-0-0-0-0")
+        time.sleep(2)
+        print("0-0-0-0-")
+        time.sleep(2)
+        print("0-0-0-0")
+        time.sleep(2)
+        print("0-0-0-")
+        time.sleep(2)
+        print("0-0-0")
+        time.sleep(2)
+        print("0-0-")
+        time.sleep(2)
+        print("0-0")
+        time.sleep(2)
+        print("0-")
+        time.sleep(2)
